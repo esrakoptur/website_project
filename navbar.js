2c51f7605181354917ba3ba2a1661f118de245cb
esra <esrakopturx@gmail.com>
2022-07-18 10:16:39 +0300

    updated nawbar

---
+++ navbar.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.backgroundColor = '#776483'
+  } else {
+    document.getElementById("navbar").style.backgroundColor = 'transparent'
+  }
+}
